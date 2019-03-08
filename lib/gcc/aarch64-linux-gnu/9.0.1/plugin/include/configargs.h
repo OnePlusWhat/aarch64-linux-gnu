94f4a8e210cc829e03905822055b474ebbf8e3e8
YousefAlgadri <yusufgadrie@gmail.com>
2019-03-08 23:46:22 +0700

    GCC-9.0.1-20190308
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>

--- lib/gcc/aarch64-linux-gnu/9.0.1/plugin/include/configargs.h
+++ lib/gcc/aarch64-linux-gnu/9.0.1/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --enable-languages=c --target=aarch64-linux-gnu --prefix=/root/gcc/aarch64-linux-gnu";
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror CFLAGS='-g0 -O2 -fstack-protector-strong' CXXFLAGS='-g0 -O2 -fstack-protector-strong' --enable-languages=c --target=aarch64-linux-gnu --prefix=/root/gcc/aarch64-linux-gnu";
 static const char thread_model[] = "posix";
 
 static const struct {
